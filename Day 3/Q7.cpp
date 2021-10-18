087819afa84c3684f7cb439beb259bbcbd4cfc1e
S-M-J-I <jishanlion@gmail.com>
2021-10-18 19:45:24 +0600

    Added Two Sum II

---
+++ Day 3/Q7.cpp
@@ -0,0 +1,31 @@
+/** Two Sum II */
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        int i=0, j=numbers.size()-1;
+        vector<int> res;
+        
+        while(i < j) {
+            if(numbers[i]+numbers[j]==target) {
+                res.push_back(i+1);
+                res.push_back(j+1);
+                return res;
+            }
+            
+            else if(numbers[i]+numbers[j] < target) {
+                i++;
+            }
+            
+            else {
+                j--;   
+            }
+        }
+        
+        return res;
+    }
+};
