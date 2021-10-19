12ab7ab433c4975bfffabef6b54baca65587775b
S-M-J-I <jishanlion@gmail.com>
2021-10-19 10:18:33 +0600

    Added Reverse String

---
+++ Day 4/Q8.cpp
@@ -0,0 +1,16 @@
+/** Reverse String */
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        for(int i = 0; i < s.size()/2; ++i) {
+            char temp = s[i];
+            s[i] = s[s.size() - 1 - i];
+            s[s.size() - 1 - i] = temp;
+        }
+        
+    }
+};
