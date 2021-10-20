fe9612eb861074ba6a619029707433145fc45fd1
S-M-J-I <jishanlion@gmail.com>
2021-10-20 10:56:52 +0600

    Added Middle of Linked List

---
+++ Day 5/10_Middle_of_linked_list.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Definition for singly-linked list.
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        ListNode* len = head;
+        int size = 0;
+        while(len) {
+            size++;
+            len = len->next;
+        }
+        
+        ListNode* curr = head;
+        for(int i = 0; i < size/2; ++i) {
+            curr = curr->next;
+        }
+        
+        return curr;
+    }
+};
